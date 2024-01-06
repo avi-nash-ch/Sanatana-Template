e4e8f21a09a0fda82a0aed4a90269571a180cbce
avi-nash-ch <chaurasiyaavinash97@gmail.com>
2024-01-06 18:03:03 +0530

    some change in Title

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -6,7 +6,7 @@
         class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
     </div>
-    <h4 class="text-center" style="padding-top: 100px;">Welcome To Sanatan Blog Application In Codeigniter</h4>
+    <h4 class="text-center" style="padding-top: 100px;">Welcome To Sanatana Blog Application In Codeigniter</h4>
     <p class="text-center" style="padding-top: 30px; font-size: 16px ;">Please choose your options from Sidebar</p>
     
 </main>
